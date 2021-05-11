88678930939bf6862d5f1d94bc9f069b2a4620bd
nikomb3 <nikotamajapan@gmail.com>
2021-05-11 21:43:06 +0900

    Django入門ブログ新機能 レッスン1 owarimade

--- app/forms.py
+++ app/forms.py
@@ -4,3 +4,5 @@ from django import forms
 class PostForm(forms.Form):
     title = forms.CharField(max_length=30, label='title')
     content = forms.CharField(label='naiyou', widget=forms.Textarea())
+    image = forms.ImageField(label='image pics', required=False)
+    
--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField("title", max_length=200)
+    image = models.ImageField(upload_to='images', verbose_name='image pics', null=True, blank=True)
     content = models.TextField("honbun")
     created = models.DateTimeField("sakusei" , default=timezone.now)
 
