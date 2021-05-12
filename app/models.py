b69c18c896f1462cb3f546df0063090715318da5
nikomb3 <nikotamajapan@gmail.com>
2021-05-12 14:32:09 +0900

    Django入門ブログ新機能 レッスン2(カテゴリ機能) category のリンクだけうまくいかない

--- app/models.py
+++ app/models.py
@@ -3,8 +3,15 @@ from django.conf import settings
 from django.utils import timezone
 
 
+class Category(models.Model):
+    name = models.CharField('category', max_length=100)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, verbose_name='category', on_delete=models.PROTECT)
     title = models.CharField("title", max_length=200)
     image = models.ImageField(upload_to='images', verbose_name='image pics', null=True, blank=True)
     content = models.TextField("honbun")
